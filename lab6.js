95dc2a53f02ea31438d6273c24b334ac5ec386b6
mirikk <43815545+mirikk@users.noreply.github.com>
2019-05-08 21:12:31 +0300

    Add files via upload

---
+++ lab6.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('URL: ' + request.url)
+}
+
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('Error:', err)
+    }
+})
